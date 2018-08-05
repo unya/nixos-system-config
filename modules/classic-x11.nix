cc4dd2dc96c1900292ba4451191866b799008bdb
Paweł Lasek <pawel.lasek@nearform.com>
2018-08-05 12:37:34 +0200

    Some more base xorg packages

--- modules/classic-x11.nix
+++ modules/classic-x11.nix
@@ -12,7 +12,13 @@
         xorg.xkill xorg.xev xorg.xdpyinfo xorg.xdm
         xorg.xset xorg.xmodmap glxinfo
         google-chrome pavucontrol mpv youtube-dl
-        wine compton xorg.xwd
+        wine compton xorg.xwd xorg.smproxy xorg.twm
+        xorg.xbacklight xorg.xauth xorg.x11perf
+        xorg.xdriinfo xorg.xinput xorg.xkbprint xorg.xkbutils
+        xorg.xlsatoms xorg.xkbcomp xorg.xkbevd xorg.xlsclients
+        xorg.xmag xorg.xmessage xorg.xorgdocs
+        xorg.xprop xorg.xrandr xorg.xrdb xorg.xrefresh
+        xorg.xsetroot xorg.xvinfo xorg.xwininfo xorg.xwud
     ];
 
     fonts = {
