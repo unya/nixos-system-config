e13681f91fa36e78e36009422e7facf1c3287771
Paweł Lasek <pawel.lasek@nearform.com>
2018-08-05 11:21:41 +0200

    A ton of common networking options

--- modules/network-common.nix
+++ modules/network-common.nix
@@ -1,5 +1,27 @@
 {pkgs, lib, ...}:
 
 {
-    
+    networking.domain = "ruri-systems.net";
+    networking.enableWLAN = true;
+    networking.firewall.enable = false;
+    networking.nftables.enable = true;
+    networking.search = ["local", "ruri-systems.net"]
+    networking.usePredictableInterfaceNames = false;
+    networking.wireless.enable = true;
+    networking.wireless.userControlled.enable = true;
+    networking.cachefilesd.enable = true;
+    services.miredo.enable = true;
+    services.avahi = {
+        enable = true;
+        ipv6 = true;
+        ipv4 = true;
+        publish = {
+            addresses = true;
+            domain = true;
+            enable = true;
+            hinfo = true;
+            userServices = true;
+            workstation = true;
+        };
+    };
 }
