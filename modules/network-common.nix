2377c30e98e2c06bdc35428bd58985f8cd632f92
Paweł Lasek <pawel.lasek@gmail.com>
2018-08-12 17:41:07 +0000

    Configured dnsmasq for possibly better DNS behaviour.

--- modules/network-common.nix
+++ modules/network-common.nix
@@ -2,6 +2,13 @@
 
 {
     networking.domain = "ruri-systems.net";
+    services.dnsmasq.enable = true;
+    services.dnsmasq.servers = ["8.8.8.8" "8.8.4.4"];
+    services.dnsmasq.resolveLocalQueries = true;
+    services.dnsmasq.extraConfig = ''
+server=/p4/192.168.7.113
+server=/p4.int/192.168.7.113
+'';
     networking.enableWLAN = true;
     networking.firewall.enable = false;
     networking.search = ["local" "ruri-systems.net"];
